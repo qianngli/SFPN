9bf9df02f57ae6a3d52ebfcdde804368bd480501
Qiang Li <liqmges@gmail.com>
2022-11-02 09:02:44 +0800

    Update noDU_option.py

--- noDU_option.py
+++ noDU_option.py
@@ -8,7 +8,7 @@ parser.add_argument("--nEpochs", type=int, default=200, help="maximum number of 
 parser.add_argument("--show", action="store_true", help="show Tensorboard")
 parser.add_argument("--lr", type=int, default=1e-4, help="lerning rate")
 parser.add_argument("--cuda", action="store_true", help="Use cuda")
-parser.add_argument("--gpus", default="0,1,2,3", type=str, help="gpu ids (default: 0)")
+parser.add_argument("--gpus", default="0,1", type=str, help="gpu ids (default: 0)")
 parser.add_argument("--threads", type=int, default=12, help="number of threads for dataloader to use")
 parser.add_argument("--resume", default="", type=str, help="Path to checkpoint (default: none)  checkpoint/model_epoch_95.pth")
 parser.add_argument("--start-epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")               
@@ -16,7 +16,7 @@ parser.add_argument("--step", type=int, default=30, help="Sets the learning rate
 
 # Network settings
 parser.add_argument("--datasetName", default="CAVE", type=str, help="data name")
-parser.add_argument("--upscale_factor", default=32, type=int, help="super resolution upscale factor")
+parser.add_argument("--upscale_factor", default=8, type=int, help="super resolution upscale factor")
 parser.add_argument('--patchSize', type=int, default=12, help='size of crop')
 parser.add_argument('--crop_num', type=int, default=64, help='number of crop')
 parser.add_argument('--interpolation', type=str, default='Kernel', help=' type of interpolation  Bicubicu, Kernal estimation')
@@ -28,6 +28,6 @@ parser.add_argument('--patchsize', type=int, default=3, help='number of bands')
 parser.add_argument('--stride', type=int, default=3, help='number of module')
 
 # Test image
-parser.add_argument('--model_name', default='result/CAVE_model_8_Kernel_2_noAgg.pth', type=str, help='super resolution model name ')
-parser.add_argument('--method', default='Fusion', type=str, help='super resolution method name')
+parser.add_argument('--model_name', default='/CAVE_model_8.pth', type=str, help='super resolution model name ')
+parser.add_argument('--method', default='SFPN', type=str, help='super resolution method name')
 opt = parser.parse_args() 
